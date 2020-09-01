e70e0ee243a2e6661609fbd53a6e624cfc271189
Kevin Wang <kcwang06@gmail.com>
2020-09-01 19:56:02 -0400

    three columns for landing page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Container, Col, Row } from 'react-bootstrap'
 
 export const Home = () => {
 	return (
 		<div className='blank'>
 			<h1>Home page </h1>
+			<Container>
+				<Row>
+					<Col>1 of 3</Col>
+					<Col>2 of 3</Col>
+					<Col>3 of 3</Col>
+				</Row>
+			</Container>
 		</div>
 	)
 }
